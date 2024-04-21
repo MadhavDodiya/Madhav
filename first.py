bb2cd9d6e58c7e29c3af4a8777060c303a2bc3a1
MadhavDodiya <madhavdodiya2017@gmail.com>
2024-04-21 10:04:52 +0530

    at classes

---
+++ first.py
@@ -0,0 +1,19 @@
+# print("Hello world")
+
+# print("Hello \nWorld")
+
+# a=2
+# b="Madhav"
+
+# print(a)
+# print(type(b))
+# print (10 + 5)
+a = 50
+b = 5
+print("This is arithimetic :", a+b)
+print("This is Subtraction :",a-b)
+print("This is Multiplication",a*b)
+print("This is a Division :",a/b)
+print("This is a Modulus :",a%b)
+print("This is a Exponentiation	:",a**b)
+print("This is a Floor division	:",a//b)
---
+++ one.py
@@ -0,0 +1,45 @@
+"""
+print("hi this is first day of python")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+print("second line")
+sdgfdb
+fdhngfn
+gfjnfgjmn
+"""
+# \n=new line
+# \t=tab
+
+# comment => singleline,multiline
+
+# variable =
+# datatype =int,string,float,boolean,complex
+
+# variablename=value
+
+# a=2
+# b=hi  
+# c=25.36587907
+# d=False
+# e=complex(3,4)
+# print(e)
+# print(type(e))
+
+# operators =>
+
+# b=input("Enter value of b :")
+
+# print("the value of b is ",b)
+# print("the value of a is ",a)
+
+a=str(input("Enter value of a :"))
+
+print(type(a))
+
+# type casting(type conversion)=> int(),float(),str(),bool()
