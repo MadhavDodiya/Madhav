7862ff84404be732169150dbd441ac016527f5d6
Madhav Dodiya 2017 <105213289+MadhavDodiya@users.noreply.github.com>
2024-04-27 19:20:46 +0530

    Add files via upload

---
+++ loop-task.py
@@ -0,0 +1,15 @@
+# 3
+# 9
+# 3 5 7 9
+
+
+a=int(input("Enter value: "))
+b=int(input("Enter value: "))
+
+# while(a<=b):
+#     if(a%2==1):
+#         print(a)
+#     a=a+1
+    
+for a in range(a<=b):
+    print(a)
---
+++ loop.py
@@ -0,0 +1,38 @@
+# looping => for,while,nested loop
+# break,continue
+
+# a=1
+# b=10
+# for a in range(a,b+1):
+#     if(a==6):
+#         continue
+#     print(a)
+
+
+
+# for a in range(10,-1,-1):
+#     print(a)
+
+# i=0
+# while(i<=10):
+#     i=i+1
+#     if(i==5):
+#         continue
+#     print(i)
+
+
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i=i-1
+
+
+# 2 variable user value
+
+# 3
+# 9
+# 3 5 7 9
+
+
+
