d45ec65e660b83c7d5415f37ac1c339b7a1ce222
Madhav Dodiya 2017 <105213289+MadhavDodiya@users.noreply.github.com>
2024-05-04 19:06:52 +0530

    Add files via upload
    classes

--- nested-loop.py
+++ nested-loop.py
@@ -60,11 +60,10 @@
 # C B A
 
 c=67
-d=66
-e=65
-for a in range(1,3):
-    for b in range(1,4):
-        print(chr(c),chr(d),chr(e))
+for a in range(1,7):
+    for b in range(67,64,-1):
+        print(chr(b),end=(" "))
+    print()
 
 
 # for a in range(1,4):
