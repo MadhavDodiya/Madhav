0276f34ea6ea78203deb3c6a8d49525950b2ce45
Madhav Dodiya 2017 <105213289+MadhavDodiya@users.noreply.github.com>
2024-05-18 19:03:14 +0530

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,90 @@
+# def myfunction(a,b):
+#     print(a+b)
+
+# myfunction()
+
+# lambda function -> 
+
+# ans=lambda a,b:(a+b)
+# print(ans(3,7))
+
+
+# error = compile
+# exception = runtime
+
+# try:
+#     print((5//0))
+# except:
+#     print("Please check your values !!")
+# else:
+#     print("DONE !!")
+# finally:
+#     print("I am DON !!")
+    
+    
+# file = text,binary     
+# mode => write(w),read(r),append(a)
+
+
+# obj=open("file1.txt","a")
+# obj.write("\n")
+# for i in range(1,11):
+#     obj.write(f"{i}")
+#     obj.write("\n")
+# obj.close()
+
+
+
+obj=open("file1.txt","r")
+# print(obj.read(5))
+for i in obj:
+    print(i)
+obj.close()
+
+
+
+# a=3
+# b=20
+# def second():
+#     print("BYE")
+# print(myfunction(a))      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# 2 variable    
+# user value
+# function pass
+# greater find
+
+# 3
+# 6
+# 6 is greater
+
+# def function(a,b):
+    
+#     if(a>b):
+#         print(f"{a} is greater")
+#     else:
+#         print(f"{b} is not greater")
+    
+# a=int(input("Enter number: "))
+# b=int(input("Enter number: "))    
+
+# function(a,b)
